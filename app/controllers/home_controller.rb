df69dea80e293907ed22e2898daa870e7e89ad53
csterritt <csterritt@gmail.com>
2016-08-26 15:05:36 -0400

    Initial employee model, user login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    # Debug
+    $stderr.puts "\n--------------------------------\n"
+    $stderr.puts "current_employee: #{current_employee.inspect}"
+    $stderr.puts "--------------------------------\n"
 
   end
 
