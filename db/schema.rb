7fdac4d0744179f57e797a80ac83d093a301529e
csterritt <csterritt@gmail.com>
2016-08-26 08:46:31 -0400

    Added Rating model, specs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826114122) do
+ActiveRecord::Schema.define(version: 20160826123717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ratings", force: :cascade do |t|
+    t.string   "giver"
+    t.string   "receiver"
+    t.string   "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
