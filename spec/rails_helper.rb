cba28f28db2654c2cb22d6a303b79bd41c0eb7d8
csterritt <csterritt@gmail.com>
2016-08-26 10:22:36 -0400

    Example feature spec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -54,6 +56,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Some capybara integration
+  config.infer_spec_type_from_file_location!
 
   # Make rspec work with devise
   config.include Devise::Test::ControllerHelpers, type: :controller
