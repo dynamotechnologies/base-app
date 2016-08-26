b78f59f0f8aa73b704ea259672660d80322721cf
csterritt <csterritt@gmail.com>
2016-08-26 08:31:29 -0400

    First user (model) specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "cannot have an empty email" do
+    user = User.new
+    expect(user.save).to be_falsey
+    expect(user.errors[:email]).to eql(["can't be blank"])
+  end
+
+  it "cannot have an empty password" do
+    user = User.new email: 'x@y.z'
+    expect(user.save).to be_falsey
+    expect(user.errors[:password]).to eql(["can't be blank"])
+  end
 end
